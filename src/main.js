210a66aee4890cac5f0542c0b1e2547c91b0efa5
jesus.fernandez <jesus.fernandez@gesthispania.com>
2024-09-03 15:02:48 +0200

    Add formkit to component

--- src/main.js
+++ src/main.js
@@ -2,7 +2,10 @@ import { createApp } from 'vue'
 import './assets/main.css'
 import App from './App.vue'
 import router from './router'
+import { plugin, defaultConfig } from '@formkit/vue'
+import config from '../formkit.config'
 
 createApp(App)
     .use(router)
+    .use(plugin, defaultConfig(config))
     .mount('#app')
--- src/views/NuevoClienteView.vue
+++ src/views/NuevoClienteView.vue
@@ -1,4 +1,5 @@
 <script setup>
+  import { FormKit } from '@formkit/vue'
   import RouterLink from '../components/ui/RouterLink.vue'
   import Heading from '../components/ui/Heading.vue'
 
@@ -7,6 +8,10 @@
       type: String
     }
   })
+
+  const handleSubmit = (data) => {
+    console.log(data)
+  }
 </script>
 
 <template>
@@ -18,4 +23,77 @@
       Volver
     </RouterLink>
   </div>
+  <div class="mx-auto mt-10 bg-white shadow rounded">
+    <div class="mx-auto md:w-2/3 py-20 px-6">
+      <FormKit
+          type="form"
+          submit-label="Añadir cliente"
+          incomplete-message="No se pudo enviar, revisa los errores"
+          @submit="handleSubmit"
+      >
+        <FormKit
+            type="text"
+            label="Nombre"
+            placeholder="Nombre"
+            validation="required"
+            :validation-messages="{ required: 'El nombre es obligatorio' }"
+            name="nombre"
+        />
+        <FormKit
+            type="text"
+            label="Apellido"
+            placeholder="Apellido"
+            validation="required"
+            :validation-messages="{ required: 'El apellido es obligatorio' }"
+            name="apellido"
+        />
+        <FormKit
+            type="email"
+            label="Email"
+            placeholder="Email"
+            validation="required|email"
+            :validation-messages="{ required: 'El email es obligatorio', email: 'Escribe un email válido' }"
+            name="email"
+        />
+        <FormKit
+            type="text"
+            label="Teléfono"
+            placeholder="Teléfono: XXXXXXXXX"
+            validation="+matches:/^[6-9]{3}[0-9]{3}[0-9]{3}/"
+            :validation-messages="{ matches: 'Escribe un teléfono correcto' }"
+            name="telefono"
+        />
+        <FormKit
+            type="text"
+            label="Empresa"
+            placeholder="Empresa"
+            name="empresa"
+        />
+        <FormKit
+            type="text"
+            label="Puesto"
+            placeholder="Puesto"
+            name="puesto"
+        />
+      </FormKit>
+    </div>
+  </div>
 </template>
+
+<style>
+  .formkit-wrapper {
+    max-width: 100%;
+    width: 100%;
+  }
+  [data-type="submit"] .formkit-input {
+    margin-top: 1rem;
+    font-weight: bold;
+    background: #059669;
+  }
+  [data-type="submit"] .formkit-input:hover {
+    background: #066044;
+  }
+  .formkit-label, .formkit-input {
+    font-family: 'Poppins', sans-serif;
+  }
+</style>
