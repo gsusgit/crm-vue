ad972135de0e094c5056b75330225d1ffa57362b
jesus.fernandez <jesus.fernandez@gesthispania.com>
2024-09-05 09:43:25 +0200

    Style customer table action buttons

--- src/components/templates/Cliente.vue
+++ src/components/templates/Cliente.vue
@@ -47,10 +47,10 @@
     <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500 ">
       <RouterLink
           :to="{ name: 'editar-cliente', params: { id: cliente.id } }"
-          class="text-teal-600 hover:text-teal-900 mr-5"
+          class="px-3 py-2 text-xs font-medium text-center inline-flex items-center text-white bg-teal-700 rounded-lg hover:bg-teal-800 focus:ring-4 focus:outline-none focus:ring-teal-300 dark:bg-teal-600 dark:hover:bg-teal-700 dark:focus:ring-teal-800 mr-5"
       >Editar</RouterLink>
       <button
-        class="text-red-600 hover:text-red-900"
+        class="px-3 py-2 text-xs font-medium text-center inline-flex items-center text-white bg-red-700 rounded-lg hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800"
         @click="$emit('borrar-cliente', cliente.id)"
       >Eliminar</button>
     </td>
